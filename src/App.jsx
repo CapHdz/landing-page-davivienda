d9c8d11deaec22c41fa53ecaae75c2d22657eca6
Maria Fernanda Isturiz <mfibarajas@gmail.com>
2024-02-21 17:53:00 -0600

    Descripción, requisitos y botón de inscripción

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 // Code: Main App component
 
+import BotonInscripcion from "./components/BotonInscripcion"
+import Descripcion from "./components/Descripcion"
+import Requisitos from "./components/Requisitos"
+
+
 function App() {
   return (
     <>
       {/* Aca iran los componentes */}
+      <Descripcion/>
+      <Requisitos/>
+      <BotonInscripcion/>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'davi-Red': '#F21D2F',
+        'davi-Blue': '#048ABF',
+        'davi-Yellow': '#F2CE1B',
+        'davi-Orange': '#F2811D',
+        'davi-Grey': '#F2F2F2',
+        500: '#64748b'
+      }
+    },
   },
   plugins: [],
 }
