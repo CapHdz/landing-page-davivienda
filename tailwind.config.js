c381c591c2ca5da022577bd230ce49a2f22bafc2
Ismar Reyes <135240830+ireyesdev@users.noreply.github.com>
2024-02-21 17:47:31 -0600

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx,html}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'davi-Red': '#f21d2f',
+        'davi-Blue': '#048abf',
+        'davi-Yellow': '#f2cE1b',
+        'davi-Orange': '#f2811d',
+        'davi-Grey': '#f2f2f2',
+      },
+    },
   },
   plugins: [],
 }
